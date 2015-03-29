f80028d6b7996ba023672ce98b3ac8a6c86b13f5
Matt Hackmann <matt@dxprog.com>
2015-03-28 21:45:32 -0700

    source of truth can now be an array of servers

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 // Source of truth URL for retrieving images
-define('IMAGE_SOT', 'http://cdn.awwni.me.s3.amazonaws.com');
+const IMAGE_SOT = [ 'http://cdn.awdwni.me/', 'http://cdn.awwni.me.s3.amazonaws.com' ];
 
 // Local storage path
-define('LOCAL_PATH', '/var/www/redditbooru-images');
+const LOCAL_PATH = '/var/www/redditbooru-images';
--- index.php
+++ index.php
@@ -27,30 +27,40 @@ function prepDirectoryPath($file) {
 
 }
 
-function getImage($fileName) {
+function getImage($fileName, $source) {
     $retVal = false;
 
     // Verify the image is OK first
-    $url = IMAGE_SOT . $fileName;
-    $headers = get_headers($url);
+    $url = $source . $fileName;
+    $headers = get_headers($url, 1);
 
     if ($headers && strpos($headers[0], '200') !== false) {
         $retVal = file_get_contents($url);
+        $retVal = (object) [
+            'data' => $retVal,
+            'contentType' => $headers['Content-Type']
+        ];
     }
 
     return $retVal;
 }
 
 $fileName = $_GET['_q'];
-$data = getImage($fileName);
+
+// Loop through all available sources until we get on that works
+foreach (IMAGE_SOT as $source) {
+    $file = getImage($fileName, $source);
+    if ($file) {
+        break;
+    }
+}
 
 // If anything is amiss, just 404
-if (!$data) {
+if (!$file) {
     header('HTTP/1.1 404 Not Found');
 } else {
     prepDirectoryPath($fileName);
-    file_put_contents(LOCAL_PATH . '/' . $fileName, $data);
-
-    // Redirect to the file (which is to say, the same URL that was requested)
-    header('Location: ' . $_SERVER['REQUEST_URI']);
+    file_put_contents(LOCAL_PATH . '/' . $fileName, $file->data);
+    header('Content-Type: ' . $file->contentType);
+    echo $file->data;
 }
