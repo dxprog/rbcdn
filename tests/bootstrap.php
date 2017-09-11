638012726364b0ea6bf545a99a3dd58f86146a19
Matt Hackmann <matt@dxprog.com>
2017-09-10 20:46:19 -0700

    refactored constants and fixed image save tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,11 @@ require('./src/util/http-helpers.php');
 require('./src/responder.php');
 require('./src/image-fetch.php');
 require('./src/mobile-fetch.php');
+
+const SOURCE_1_PNG = '/anime1.png';
+const SOURCE_2_JPG = '/tsunderes/anime2.jpg';
+const SOURCE_1_GIF = '/anime3.gif';
+const SOURCES = [
+  './tests/sources/chitoge.cdn.awwni.me',
+  './tests/sources/taiga.cdn.awwni.me'
+];
--- tests/image-fetch-test.php
+++ tests/image-fetch-test.php
@@ -3,18 +3,8 @@
 use PHPUnit\Framework\TestCase;
 
 final class ImageFetchTest extends TestCase {
-  static $sources = [
-    './tests/sources/chitoge.cdn.awwni.me',
-    './tests/sources/taiga.cdn.awwni.me'
-  ];
 
   private $storagePath;
-
-  // First source has this image
-  const IMAGE_1 = '/anime1.png';
-
-  // Second source has this image
-  const IMAGE_2 = '/tsunderes/anime2.jpg';
 
   function setUp() {
     $tmpDir = tempnam(sys_get_temp_dir(), 'rbcdn-tests');
@@ -30,39 +20,39 @@ final class ImageFetchTest extends TestCase {
   }
 
   function testFetchesFromFirstSource() {
-    $instance = new ImageFetch(self::IMAGE_1, self::$sources, $this->storagePath);
+    $instance = new ImageFetch(SOURCE_1_PNG, SOURCES, $this->storagePath);
     $this->assertTrue($instance->fetch(), 'image was not found');
     $this->assertEquals($instance->contentType, 'image/png', 'Content-Type is different from disk');
-    $this->assertEquals(md5($instance->imageData), md5_file(self::$sources[0] . self::IMAGE_1), 'loaded image data different from disk');
+    $this->assertEquals(md5($instance->imageData), md5_file(SOURCES[0] . SOURCE_1_PNG), 'loaded image data different from disk');
   }
 
   function testFetchesFromSecondSource() {
-    $instance = new ImageFetch(self::IMAGE_2, self::$sources, $this->storagePath);
+    $instance = new ImageFetch(SOURCE_2_JPG, SOURCES, $this->storagePath);
     $this->assertTrue($instance->fetch(), 'image was not found');
     $this->assertEquals($instance->contentType, 'image/jpeg', 'Content-Type is different from disk');
-    $this->assertEquals(md5($instance->imageData), md5_file(self::$sources[1] . self::IMAGE_2), 'loaded image data different from disk');
+    $this->assertEquals(md5($instance->imageData), md5_file(SOURCES[1] . SOURCE_2_JPG), 'loaded image data different from disk');
   }
 
   function testReturnsFalseForUnfoundImage() {
-    $instance = new ImageFetch('/some-mage-up-nonsense.jpg', self::$sources, $this->storagePath);
+    $instance = new ImageFetch('/some-mage-up-nonsense.jpg', SOURCES, $this->storagePath);
     $this->assertFalse($instance->fetch(), 'image marked as found when file does not exist');
   }
 
   function testSavesTheFetchedImage() {
-    $instance = new ImageFetch(self::IMAGE_1, self::$sources, $this->storagePath);
+    $instance = new ImageFetch(SOURCE_1_PNG, SOURCES, $this->storagePath);
     if ($instance->fetch()) {
       $instance->save();
-      $this->assertEquals(md5($instance->imageData), md5_file($this->storagePath . self::IMAGE_1), 'saved image data differs from source');
+      $this->assertEquals(md5_file(SOURCES[0] . SOURCE_1_PNG), md5_file($this->storagePath . SOURCE_1_PNG), 'saved image data differs from source');
     } else {
       $this->assertTrue(false, 'unable to load first source image');
     }
   }
 
   function testSavesTheFetchedImageWithPath() {
-    $instance = new ImageFetch(self::IMAGE_2, self::$sources, $this->storagePath);
+    $instance = new ImageFetch(SOURCE_2_JPG, SOURCES, $this->storagePath);
     if ($instance->fetch()) {
       $instance->save();
-      $this->assertEquals(md5($instance->imageData), md5_file($this->storagePath . self::IMAGE_2), 'saved image data differs from source');
+      $this->assertEquals(md5_file(SOURCES[1] . SOURCE_2_JPG), md5_file($this->storagePath . SOURCE_2_JPG), 'saved image data differs from source');
     } else {
       $this->assertTrue(false, 'unable to load second source image');
     }
